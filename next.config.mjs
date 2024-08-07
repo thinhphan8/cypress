38edda85ec1d172355cf2e04f39f5f023824d583
SOMETHINGG\thinh <thinhphanvn8499@gmail.com>
2024-08-07 21:00:06 +0700

    Add Image Domain Authorization

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
     // Server Actions are available by default now, `experimental.serverActions` option can be safely removed.
     // experimental: {
     //     serverActions: true
-    // }
+    // },
+    images: {
+        domains: ['sfipxzfhhzxxhlhuckgb.supabase.co'],
+    },
 };
 
 export default nextConfig;
