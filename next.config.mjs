dcb3b00ee7a96482c97d0912261bea11271cc34b
SOMETHINGG\thinh <thinhphanvn8499@gmail.com>
2024-08-07 12:31:52 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // Server Actions are available by default now, `experimental.serverActions` option can be safely removed.
+    // experimental: {
+    //     serverActions: true
+    // }
+};
 
 export default nextConfig;
