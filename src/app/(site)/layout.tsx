a81bdefd58f21468d38f70c7933ce0e1140bf98e
SOMETHINGG\thinh <thinhphanvn8499@gmail.com>
2024-08-07 13:20:32 +0700

    Update Landing page with Header

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from "@/components/landing-page/Header";
 
 const HomePageLayout = ({children}: { children: React.ReactNode }) => {
     return (
         <main>
+            <Header/>
             {children}
         </main>
     );
