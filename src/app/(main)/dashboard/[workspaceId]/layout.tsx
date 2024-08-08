d633722aeae756545b2eae9d0b01499020ed9cbb
SOMETHINGG\thinh <thinhphanvn8499@gmail.com>
2024-08-08 23:07:38 +0700

    Fix overflow in Dashboard page

--- src/app/(main)/dashboard/[workspaceId]/layout.tsx
+++ src/app/(main)/dashboard/[workspaceId]/layout.tsx
@@ -22,7 +22,7 @@ const Layout: React.FC<LayoutProps> = async ({children, params}) => {
                 border-l-[1px]
                 w-full
                 relative
-                overflow-scroll"
+                overflow-auto"
             >
                 {children}
             </div>
